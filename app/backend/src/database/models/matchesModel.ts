e4b13c0b6bd1fadb92ea580daf62d687bdc3067e
Brenno-Lucas <brennolucas8@gmail.com>
2023-01-23 12:10:17 -0300

    Creation of dockerfiles, migrations and models (Matches, Teams and users).

---
+++ app/backend/src/database/models/matchesModel.ts
@@ -0,0 +1,51 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import TeamsModel from './teamsModel';
+
+class MatchesModel extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+MatchesModel.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  homeTeamId: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+MatchesModel.hasMany(TeamsModel, { foreignKey: 'home_team_id', as: 'home_team' });
+MatchesModel.hasMany(TeamsModel, { foreignKey: 'away_team_id', as: 'away_team' });
+
+export default MatchesModel;
